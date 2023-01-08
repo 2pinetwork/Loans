5d4ad37dab502130b653bff9e153bc57952772d8
Batudo <batudo@2pi.network>
2023-01-08 19:11:13 -0300

    PiAdmin and SafeBox purpose and function comments

--- contracts/PiAdmin.sol
+++ contracts/PiAdmin.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+/**
+ * @title PiAdmin
+ *
+ * @dev This contract is used to check if the caller is an admin.
+ */
 abstract contract PiAdmin is AccessControl, ReentrancyGuard {
+    /**
+     * @dev Throws if caller is not an admin.
+     */
     error NotAdmin();
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /**
+     * @dev Modifier to check if the caller is an admin.
+     */
     modifier onlyAdmin() {
         if (! hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) revert NotAdmin();
         _;
