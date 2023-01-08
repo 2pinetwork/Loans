7f0f6d41357f7159f50adf51b63ea13203d2ba5b
Batudo <82539708+batudo@users.noreply.github.com>
2023-01-08 18:55:08 -0300

    LiquidityPool purpose and function comments (#30)

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
@@ -17,6 +16,13 @@ import "../interfaces/IOracle.sol";
 import "../interfaces/IPiGlobal.sol";
 import "../libraries/Errors.sol";
 
+/**
+ * @title LiquidityPool
+ *
+ * @dev Manages the liquidity for a given asset. If a given borrower wants to
+ * take a loan, this would be the contract that would be used to provide the
+ * necessary liquidity.
+ */
 contract LiquidityPool is Pausable, PiAdmin {
     using SafeERC20 for IERC20Metadata;
 
@@ -58,14 +64,49 @@ contract LiquidityPool is Pausable, PiAdmin {
     // Debt settler
     IDebtSettler public debtSettler;
 
+    /**
+     * @dev Throws when some parameter is changed after some debt has been issued.
+     */
     error AlreadyInitialized();
+
+    /**
+     * @dev Throws when due date is already passed.
+     */
     error DueDateInThePast();
+
+    /**
+     * @dev Throws when the pool is not active because it has been expired.
+     */
     error ExpiredPool();
+
+    /**
+     * @dev Throws when a given address wants to borrow more than allowed by the collateral.
+     */
     error InsufficientFunds();
+
+    /**
+     * @dev Throws when asked to borrow more than the pool can provide.
+     */
     error InsufficientLiquidity();
+
+    /**
+     * @dev Throws when trying to pay a debt that does not exist.
+     */
     error NoDebt();
+
+    /**
+     * @dev Throws when calling a function that is not from a valid collateral pool.
+     */
     error UnknownSender();
 
+    // constructor
+    /**
+     * @dev Initializes the contract.
+     *
+     * @param _piGlobal The address of the PiGlobal contract.
+     * @param _asset The address of the asset to be used on this pool.
+     * @param _dueDate The timestamp of the due date until this pool will be active.
+     */
     constructor(IPiGlobal _piGlobal, IERC20Metadata _asset, uint _dueDate) {
         if (_dueDate <= block.timestamp) revert DueDateInThePast();
         if (address(_piGlobal) == address(0)) revert Errors.ZeroAddress();
@@ -85,35 +126,144 @@ contract LiquidityPool is Pausable, PiAdmin {
         piGlobal = _piGlobal;
     }
 
+    /**
+     * @dev Throws when the pool is not active because it expired.
+     */
     modifier notExpired() {
         if (expired()) revert ExpiredPool();
         _;
     }
 
+    /**
+     * @dev Throws when the caller is not a valid collateral pool.
+     */
     modifier fromCollateralPool {
         if (! piGlobal.isValidCollateralPool(msg.sender)) revert UnknownSender();
         _;
     }
 
+    /**
+     * @dev Emitted when a user deposits funds.
+     *
+     * @param _sender The address of the account that initiated the transaction.
+     * @param _onBehalfOf The address of the user on behalf of which the funds were deposited.
+     * @param _amount The amount of funds deposited.
+     * @param _shares The amount of liquidity tokens minted.
+     */
     event Deposit(address _sender, address _onBehalfOf, uint _amount, uint _shares);
+
+    /**
+     * @dev Emitted when a user withdraws funds.
+     *
+     * @param _sender The address of the account that initiated the transaction.
+     * @param _to The address of the user to which the funds were withdrawn.
+     * @param _amount The amount of funds withdrawn.
+     * @param _shares The amount of liquidity tokens burned.
+     */
     event Withdraw(address _sender, address _to, uint _amount, uint _shares);
+
+    /**
+     * @dev Emitted when a user borrows funds.
+     *
+     * @param _sender The address of the account that initiated the transaction.
+     * @param _amount The amount of funds borrowed.
+     */
     event Borrow(address _sender, uint _amount);
+
+    /**
+     * @dev Emitted when a user repays funds.
+     *
+     * @param _sender The address of the account that initiated the transaction.
+     * @param _amount The amount of funds repaid.
+     */
     event Repay(address _sender, uint _amount);
+
+    /**
+     * @dev Emitted when the originator fee is changed.
+     *
+     * @param _oldFee The old originator fee.
+     * @param _newFee The new originator fee.
+     */
     event NewOriginatorFee(uint _oldFee, uint _newFee);
+
+    /**
+     * @dev Emitted when the interest rate is changed.
+     *
+     * @param _oldInterestRate The old interest rate.
+     * @param _newInterestRate The new interest rate.
+     */
     event NewInterestRate(uint _oldInterestRate, uint _newInterestRate);
+
+    /**
+     * @dev Emitted when the oracle is changed.
+     *
+     * @param _oldOracle The old oracle.
+     * @param _newOracle The new oracle.
+     */
     event NewOracle(address _oldOracle, address _newOracle);
+
+    /**
+     * @dev Emitted when the Pi fee is changed.
+     *
+     * @param _oldFee The old Pi fee.
+     * @param _newFee The new Pi fee.
+     */
     event NewPiFee(uint _oldFee, uint _newFee);
+
+    /**
+     * @dev Emitted when the treasury is changed.
+     *
+     * @param _oldTreasury The old treasury.
+     * @param _newTreasury The new treasury.
+     */
     event NewTreasury(address _oldTreasury, address _newTreasury);
+
+    /**
+     * @dev Emitted when fees are collected.
+     *
+     * @param _fee The amount of fees collected.
+     */
     event CollectedFee(uint _fee);
+
+    /**
+     * @dev Emitted when originator fees are collected.
+     *
+     * @param _fee The amount of originator fees collected.
+     */
     event CollectedOriginatorFee(uint _fee);
+
+    /**
+     * @dev Emitted when the safe box state is changed.
+     *
+     * @param _safeBox The address of the safe box.
+     * @param _newState The new state of the safe box.
+     */
     event SafeBoxChanged(address _safeBox, bool _newState);
+
+    /**
+     * @dev Emitted when the debt settler is changed.
+     *
+     * @param _old The old debt settler.
+     * @param _new The new debt settler.
+     */
     event NewDebtSettler(address _old, address _new);
 
     /*********** COMMON FUNCTIONS ***********/
+
+    /**
+     * @dev Returns the decimals of the underlying asset.
+     *
+     * @return The decimals of the asset.
+     */
     function decimals() public view returns (uint8) {
         return asset.decimals();
     }
 
+    /**
+     * @dev Sets the interest rate.
+     *
+     * @param _newInterestRate The new interest rate.
+     */
     function setInterestRate(uint _newInterestRate) external onlyAdmin nonReentrant {
         if (_newInterestRate > MAX_RATE) revert Errors.GreaterThan("MAX_RATE");
         if (dToken.totalSupply() > 0) revert AlreadyInitialized();
@@ -123,6 +273,11 @@ contract LiquidityPool is Pausable, PiAdmin {
         interestRate = _newInterestRate;
     }
 
+    /**
+     * @dev Sets the originator fee.
+     *
+     * @param _newOriginatorFee The new originator fee.
+     */
     function setOriginatorFee(uint _newOriginatorFee) external onlyAdmin nonReentrant {
         // No more than 100% JIC
         if (_newOriginatorFee > MAX_RATE) revert Errors.GreaterThan("MAX_RATE");
@@ -133,6 +288,11 @@ contract LiquidityPool is Pausable, PiAdmin {
         originatorFee = _newOriginatorFee;
     }
 
+    /**
+     * @dev Sets the Pi fee.
+     *
+     * @param _piFee The new Pi fee.
+     */
     function setPiFee(uint _piFee) external onlyAdmin nonReentrant {
         if (_piFee > MAX_RATE) revert Errors.GreaterThan("MAX_RATE");
         if (dToken.totalSupply() > 0) revert AlreadyInitialized();
@@ -142,6 +302,11 @@ contract LiquidityPool is Pausable, PiAdmin {
         piFee = _piFee;
     }
 
+    /**
+     * @dev Sets the treasury.
+     *
+     * @param _treasury The new treasury.
+     */
     function setTreasury(address _treasury) external onlyAdmin nonReentrant {
         if (_treasury == address(0)) revert Errors.ZeroAddress();
         if (_treasury == treasury) revert Errors.SameValue();
@@ -151,6 +316,11 @@ contract LiquidityPool is Pausable, PiAdmin {
         treasury = _treasury;
     }
 
+    /**
+     * @dev Sets the debt settler.
+     *
+     * @param _debtSettler The new debt settler.
+     */
     function setDebtSettler(IDebtSettler _debtSettler) external onlyAdmin {
         if (address(_debtSettler) == address(0)) revert Errors.ZeroAddress();
         if (address(_debtSettler) == address(debtSettler)) revert Errors.SameValue();
@@ -160,6 +330,13 @@ contract LiquidityPool is Pausable, PiAdmin {
         debtSettler = _debtSettler;
     }
 
+    // set safe box enabled/disabled
+    /**
+     * @dev Sets the safe box state. If true, the safe box is enabled and
+     * created if it doesn't exist. If false, the safe box is disabled.
+     *
+     * @param _newState The new state of the safe box.
+     */
     function setSafeBoxEnabled(bool _newState) external onlyAdmin nonReentrant {
         if (safeBoxEnabled == _newState) revert Errors.SameValue();
 
@@ -181,23 +358,52 @@ contract LiquidityPool is Pausable, PiAdmin {
         emit SafeBoxChanged(address(safeBox), safeBoxEnabled);
     }
 
+    /**
+     * @dev Returns true if this pool is expired.
+     *
+     * @return True if this pool is expired, false otherwise.
+     */
     function expired() public view returns (bool) {
         return block.timestamp > dueDate;
     }
 
+    /**
+     * @dev Returns the current asset balance of the pool.
+     *
+     * @return The current asset balance of the pool.
+     */
     function balance() public view returns (uint) {
         return asset.balanceOf(address(this));
     }
 
     /*********** LIQUIDITY FUNCTIONS ***********/
+
+    /**
+     * @dev Returns the liquidity token balance of the account.
+     *
+     * @param _account The account to check.
+     *
+     * @return The balance of the account.
+     */
     function balanceOf(address _account) public view returns (uint) {
         return lToken.balanceOf(_account);
     }
 
+    /**
+     * @dev Deposits liquidity on behalf of the user.
+     *
+     * @param _amount The amount of liquidity to deposit.
+     * @param _onBehalfOf The account to deposit on behalf of.
+     */
     function deposit(uint _amount, address _onBehalfOf) external nonReentrant notExpired {
         _deposit(_amount, _onBehalfOf);
     }
 
+    /**
+     * @dev Deposits liquidity on behalf of the sender.
+     *
+     * @param _amount The amount of liquidity to deposit.
+     */
     function deposit(uint _amount) external nonReentrant notExpired {
         _deposit(_amount, msg.sender);
     }
@@ -224,14 +430,34 @@ contract LiquidityPool is Pausable, PiAdmin {
         emit Deposit(msg.sender, _onBehalfOf, _amount, _shares);
     }
 
+    /**
+     * @dev Withdraws liquidity on behalf of the user.
+     *
+     * @param _shares The amount of liquidity to withdraw.
+     * @param _to The account to withdraw to.
+     *
+     * @return The amount of liquidity withdrawn.
+     */
     function withdraw(uint _shares, address _to) external nonReentrant returns (uint) {
         return _withdraw(_shares, _to);
     }
 
+    /**
+     * @dev Withdraws liquidity on behalf of the sender.
+     *
+     * @param _shares The amount of liquidity to withdraw.
+     *
+     * @return The amount of liquidity withdrawn.
+     */
     function withdraw(uint _shares) external nonReentrant returns (uint) {
         return _withdraw(_shares, msg.sender);
     }
 
+    /**
+     * @dev Withdraws all liquidity on behalf of the sender.
+     *
+     * @return The amount of liquidity withdrawn.
+     */
     function withdrawAll() external nonReentrant returns (uint) {
         return _withdraw(lToken.balanceOf(msg.sender), msg.sender);
     }
@@ -261,6 +487,12 @@ contract LiquidityPool is Pausable, PiAdmin {
 
     /*********** BORROW FUNCTIONS  *********/
 
+    /**
+     * @dev Borrows asset from the pool. It mints the debt and interest tokens
+     * and registers the timestamp of the event.
+     *
+     * @param _amount The amount of asset to borrow.
+     */
     function borrow(uint _amount) external nonReentrant whenNotPaused notExpired {
         if (_amount <= 0) revert Errors.ZeroAmount();
         if (_amount > balance()) revert InsufficientLiquidity();
@@ -294,21 +526,45 @@ contract LiquidityPool is Pausable, PiAdmin {
         emit Borrow(msg.sender, _amount);
     }
 
+    /**
+     * @dev Repays the debt of the sender.
+     *
+     * @param _amount The amount of asset to repay.
+     */
     function repay(uint _amount) external nonReentrant {
         _repay(msg.sender, msg.sender, _amount);
     }
 
+    /**
+     * @dev Repays the debt of the account using the sender's asset.
+     *
+     * @param _account The account to repay the debt of.
+     * @param _amount The amount of asset to repay.
+     */
     function repayFor(address _account, uint _amount) external nonReentrant {
         _repay(msg.sender, _account, _amount);
     }
 
-    // Only collateralPools can call this
-    // if anyone could call this, any unlimited allowance to this contract could be used to repay
-    // debt on behalf of anyone...
+    /**
+     * @dev Repays the debt of the account using the payer's asset.
+     * Only collateral pools can call this, if anyone could an unlimited
+     * allowance can be used to repay debt on behalf of anyone.
+     *
+     * @param _payer The account which pays the debt.
+     * @param _account The account to repay the debt of.
+     * @param _amount The amount of asset to repay.
+     */
     function liquidate(address _payer, address _account, uint _amount) external nonReentrant fromCollateralPool {
         _repay(_payer, _account, _amount);
     }
 
+    /**
+     * @dev Builds the mapping of debt to be repaid proportionally to the
+     * current borrowers for the given amount. After that, debtSettler.pay()
+     * should be called to actually pay the debt.
+     *
+     * @param _amount The amount of asset to repay.
+     */
     function buildMassiveRepay(uint _amount) external nonReentrant {
         asset.safeTransferFrom(msg.sender, address(debtSettler), _amount);
         debtSettler.build(_amount);
@@ -391,22 +647,44 @@ contract LiquidityPool is Pausable, PiAdmin {
         emit Repay(_account, _amount);
     }
 
+    /**
+     * @dev Calculates the amount of asset that the given account owes.
+     *
+     * @param _account The account to calculate the debt of.
+     *
+     * @return The amount of asset that the given account owes.
+     */
     function debt(address _account) external view returns (uint) {
         (,,, uint _amount) = _debt(_account);
 
         return _amount;
     }
 
+    /**
+     * @dev Calculates the amount of asset that the sender account owes.
+     *
+     * @return The amount of asset that the sender account owes.
+     */
     function debt() external view returns (uint) {
         (,,, uint _amount) = _debt(msg.sender);
 
         return _amount;
     }
 
+    /**
+     * @dev Calculates the total amount of asset that the pool has lent.
+     * Trivial implementation, just returns the total supply of debt and
+     * interest tokens.
+     *
+     * @return The total amount of asset that the pool has lent.
+     */
     function totalDebt() external view returns (uint) {
         return dToken.totalSupply() + iToken.totalSupply();
     }
 
+    /**
+     * @dev Toggles the pause state of the pool.
+     */
     function togglePause() external onlyAdmin {
         paused() ? _unpause() : _pause();
     }
