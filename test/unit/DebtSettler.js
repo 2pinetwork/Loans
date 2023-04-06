6f54b2c0e795e0d6602d581c2759655ad326176c
Néstor Coppi <nestorcoppi@gmail.com>
2023-04-06 15:18:29 -0300

    Lint

--- test/unit/DebtSettler.js
+++ test/unit/DebtSettler.js
@@ -595,10 +595,10 @@ describe('Debt settler', async function () {
       buildReceipt = await buildTx.wait()
       // expect(buildReceipt.gasUsed).to.be.lessThan(5e6) // last only process 50 borrowers so should be left a few
       console.log(`Balance of Settler: ${(await token.balanceOf(debtSettler.address)).toString()}`);
-      let buildReceipt2 = await (await debtSettler.pay({gasLimit: 10e6})).wait()
+      await (await debtSettler.pay({gasLimit: 10e6})).wait()
       // expect(buildReceipt2.gasUsed).to.be.greaterThan(8e6)
       console.log(`Balance of Settler: ${(await token.balanceOf(debtSettler.address)).toString()}`);
-      buildReceipt2 = await (await debtSettler.pay({gasLimit: 10e6})).wait()
+      await (await debtSettler.pay({gasLimit: 10e6})).wait()
       // expect(buildReceipt2.gasUsed).to.be.lessThan(2e6)
       // this one will run without paying
       console.log(`Balance of Settler: ${(await token.balanceOf(debtSettler.address)).toString()}`);
@@ -768,10 +768,10 @@ describe('Debt settler', async function () {
       buildReceipt = await buildTx.wait()
       // expect(buildReceipt.gasUsed).to.be.lessThan(5e6) // last only process 50 borrowers so should be left a few
       console.log(`Balance of Settler: ${(await token.balanceOf(debtSettler.address)).toString()}`);
-      let buildReceipt2 = await (await debtSettler.pay({gasLimit: 10e6})).wait()
+      await (await debtSettler.pay({gasLimit: 10e6})).wait()
       // expect(buildReceipt2.gasUsed).to.be.greaterThan(8e6)
       console.log(`Balance of Settler: ${(await token.balanceOf(debtSettler.address)).toString()}`);
-      buildReceipt2 = await (await debtSettler.pay({gasLimit: 10e6})).wait()
+      await (await debtSettler.pay({gasLimit: 10e6})).wait()
       // expect(buildReceipt2.gasUsed).to.be.lessThan(2e6)
       // this one will run without paying
       console.log(`Balance of Settler: ${(await token.balanceOf(debtSettler.address)).toString()}`);
